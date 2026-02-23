c9fa77260dbcc9757a5cb767add1217405b6de19
Mohamed Elyass <mohamedwathig3@gmail.com>
2026-02-23 06:38:59 +0200

    implement peroformance section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import ProductViewer from "./components/ProductViewer";
 import gsap from 'gsap';
 import {ScrollTrigger,SplitText } from "gsap/all"
 import ShowCase from "./components/ShowCase";
+import Performance from "./components/Performance";
+import Features from "./components/Features";
+import HighLights from "./components/HighLights";
+import Fotter from "./components/Fotter";
 function App() {
   return (
     <div>
@@ -12,6 +16,10 @@ function App() {
       <NavBar/>
       <ProductViewer/>
       <ShowCase/>
+      <Performance/>
+      <Features/>
+      <HighLights/>
+      <Fotter/>
     </div>
   );
 }
