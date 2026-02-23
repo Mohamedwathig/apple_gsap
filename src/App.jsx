9b944095b8844824a0d07b4169fcc2497e40dd24
Mohamed Elyass <mohamedwathig3@gmail.com>
2026-02-23 02:21:30 +0200

    animated section added M4

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Hero from "./components/Hero";
 import ProductViewer from "./components/ProductViewer";
 import gsap from 'gsap';
 import {ScrollTrigger,SplitText } from "gsap/all"
+import ShowCase from "./components/ShowCase";
 function App() {
   return (
     <div>
       <Hero/>
       <NavBar/>
       <ProductViewer/>
+      <ShowCase/>
     </div>
   );
 }
